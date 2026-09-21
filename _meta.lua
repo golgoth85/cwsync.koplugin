5f78c8359fd212a775345a8ea6f2ffbc6e72ddd1
golgoth85 <marco.palandri@gmail.com>
2026-09-21 08:21:27 +0200

    Bump CWSync to 1.0.1

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
     name = "cwsync",
     fullname = _("CWSync for Calibre-Web NextGen"),
     description = _([[Synchronizes reading progress, device books and highlights with Calibre-Web NextGen, and preserves CWNG shelf order in native KOReader Collections.]]),
-    version = "1.0.0",
+    version = "1.0.1",
 }
